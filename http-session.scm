c8b4fb171c1ca1eeb35ae1adc6b425c2d7ea0b18
Mario Domenech Goulart <mario.goulart@gmail.com>
2010-01-12 22:11:12 +0000

    * `make-session-table' is now exported (it was `make-table' internally).
    * `current-url' and `current-ip' are gone
    
    * `session-create' doesn't use the url-pattern argument
      anymore (`session-table' can be parameterized for separating
      web applications)
    
    * added `match-ip-address?' parameter.  By default, http-session
      ignores the IP address of the client when checking whether the
      session is valid.  If `match-ip-address?' is #t, it will take
      the IP address into account.
    
    * Deprecated procedures: `session-delete-binding!' and
      `session-delete!'
    
    * New procedures: `session-del!' (to substitute
      `session-delete-binding!') and `session-destroy!' (to
      substitute `session-delete!')
    
    * Now session items are internally represented by session-item
      objects (records).
    
    * Bumped version to 2.0.0

--- http-session.scm
+++ http-session.scm
@@ -2,16 +2,19 @@
         (session-table session-create session-refresh! session-valid?
          session-delete! session-ref session-set!
          session-bindings session-delete-binding!
-         current-ip current-url session-lifetime session-id-generator)
+         session-lifetime session-id-generator
+         make-session-table match-ip-address? session-destroy! session-del!)
 
   (import scheme chicken data-structures utils extras regex)
 
   (use sha1 posix intarweb spiffy uri-common srfi-1 srfi-18 srfi-69)
 
-  (define (make-table) (make-hash-table equal?))
-  ;; key=sid value=(expiration from-ip url-pattern bindings)
+  (define (make-session-table) (make-hash-table equal?))
+  ;; key=sid value=#<session-item expiration from-ip bindings>
 
-  (define session-table (make-parameter (make-table)))
+  (define session-table (make-parameter (make-session-table)))
+
+  (define match-ip-address? (make-parameter #f))
 
   (define session-lifetime (make-parameter 3600)) ; 1h
 
@@ -19,16 +22,26 @@
     (+ (current-milliseconds)
        (inexact->exact (floor (* (session-lifetime) 1000)))))
 
-  (define (session-create url-pattern #!optional bindings)
+  (define-record session-item expiration ip bindings)
+
+  (define (get-session-item sid #!optional (error? #t))
+    (handle-exceptions
+     exn
+     (if error?
+         (error (conc "Invalid session ID: " sid))
+         #f)
+     (hash-table-ref (session-table) sid)))
+    
+  (define (session-create #!optional (bindings '()))
     (let ((sid (unique-id))
           (expiration (expiration)))
-      (hash-table-set! (session-table) sid (list expiration (current-ip) url-pattern bindings))
+      (hash-table-set! (session-table) sid (make-session-item expiration (remote-address) bindings))
       (thread-start!
        (make-thread
         (lambda ()
           (let loop ()
-            (let* ((expiration (car (hash-table-ref (session-table) sid)))
-                   (timeout (- expiration (current-milliseconds))))
+            (let* ((sitem (get-session-item sid))
+                   (timeout (- (session-item-expiration sitem) (current-milliseconds))))
               (when (> timeout 0)
                     (thread-sleep! (/ timeout 1000))
                     (loop))))
@@ -36,21 +49,20 @@
       sid))
 
   (define (session-refresh! sid)
-    (and-let* ((session (hash-table-ref/default (session-table) sid #f))
-               (expiration (expiration)))
-              (set-car! session expiration)))
+    (session-item-expiration-set! (get-session-item sid) (expiration)))
 
   (define (session-valid? sid)
-    (and-let* ((sid sid)
-               (session (hash-table-ref/default (session-table) sid #f))
-               (ip (current-ip))
-               (accessed-url (current-url))
-               (url-pattern (caddr session)))
-              (and (equal? ip (cadr session))
-                   (string-match url-pattern accessed-url))))
+    (not (not (and sid
+                   (and-let* ((sitem (get-session-item sid #f)))
+                     (if (match-ip-address?)
+                         (equal? (remote-address) (session-item-ip sitem))
+                         #t))))))
 
-  (define (session-delete! sid)
+  (define (session-destroy! sid)
     (hash-table-delete! (session-table) sid))
+  
+  (define session-delete!  ;; DEPRECATED
+    session-destroy!)
 
   (define session-id-generator
     (make-parameter
@@ -66,41 +78,25 @@
           id)))
 
   (define (session-ref sid var #!optional default)
-    (if (session-valid? sid)
-        (let ((bindings (last (hash-table-ref/default (session-table) sid '()))))
-          (or (alist-ref var bindings) default))
-        (error "Invalid session ID.")))
-
+    (or (alist-ref var (session-item-bindings (get-session-item sid)))
+        default))
+  
   (define (session-set! sid var val)
-    (define (binding-set! session var val)
-      (set! session (reverse session))
-      (set-car! session (if (car session)
-                            (let ((b (assq var (car session))))
-                              (if b
-                                  (begin
-                                    (set-cdr! b val)
-                                    (car session))
-                                  (append (car session) (list (cons var val)))))
-                            (list (cons var val))))
-      (set! session (reverse session))
-      session)
-    (if (session-valid? sid)
-        (let* ((session (hash-table-ref/default (session-table) sid #f)))
-          (hash-table-set! (session-table) sid (binding-set! session var val)))
-        (error "Invalid session ID.")))
+    (let ((sitem (get-session-item sid)))
+      (let ((new-bindings (alist-update! var val (session-item-bindings sitem))))
+        (hash-table-set! (session-table)
+                         sid
+                         (make-session-item (expiration)
+                                            (session-item-ip sitem)
+                                            new-bindings)))))
 
   (define (session-bindings sid)
-    (if (session-valid? sid)
-        (last (hash-table-ref/default (session-table) sid '()))
-        (error "Invalid session ID.")))
-
-  (define (session-delete-binding! sid var)
+    (session-item-bindings (get-session-item sid)))
+  
+  (define (session-del! sid var)
     (alist-delete! var (session-bindings sid)))
 
-  (define (current-ip)
-    (remote-address))
-
-  (define (current-url)
-    (uri->string (request-uri (current-request))))
-
+  (define session-delete-binding! ;; DEPRECATED
+    session-del!)
+  
   )
