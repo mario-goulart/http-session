022506d12bf72be0443f4b5f343f19b50301381e
Mario Domenech Goulart <mario.goulart@gmail.com>
2011-09-15 18:53:02 +0000

    http-session: removed module indentation & whitespace cleanup

--- http-session.scm
+++ http-session.scm
@@ -10,140 +10,139 @@
          session-storage-delete! session-storage-cleanup!
          )
 
-  (import scheme chicken data-structures utils extras)
+(import scheme chicken data-structures utils extras)
 
-  (use simple-sha1 posix intarweb spiffy uri-common srfi-1 srfi-18 srfi-69)
+(use simple-sha1 posix intarweb spiffy uri-common srfi-1 srfi-18 srfi-69)
 
 
-  ;; Configurable storage backend API
-  (define session-storage-initialize
-    (make-parameter
-     (lambda ()
-       (make-hash-table equal?))))
+;; Configurable storage backend API
+(define session-storage-initialize
+  (make-parameter
+   (lambda ()
+     (make-hash-table equal?))))
 
-  (define session-storage-set!
-    (make-parameter
-     (lambda (session-table sid session-item)
-       (hash-table-set! session-table sid session-item))))
+(define session-storage-set!
+  (make-parameter
+   (lambda (session-table sid session-item)
+     (hash-table-set! session-table sid session-item))))
 
-  (define session-storage-ref
-    (make-parameter
-     (lambda (session-table sid)
-       (hash-table-ref session-table sid))))
+(define session-storage-ref
+  (make-parameter
+   (lambda (session-table sid)
+     (hash-table-ref session-table sid))))
 
-  (define session-storage-delete!
-    (make-parameter
-     (lambda (session-table sid)
-       (hash-table-delete! session-table sid))))
+(define session-storage-delete!
+  (make-parameter
+   (lambda (session-table sid)
+     (hash-table-delete! session-table sid))))
 
-  (define session-storage-cleanup!
-    (make-parameter
-     (lambda ()
-       (void))))
-  
-  ;; Session items
-  
-  (define-record session-item expiration ip bindings finalizer)
+(define session-storage-cleanup!
+  (make-parameter
+   (lambda ()
+     (void))))
 
-  (define (get-session-item sid #!optional (error? #t))
-    (handle-exceptions
-     exn
-     (if error?
-         (raise (make-composite-condition
-                 (make-property-condition
-                  'exn
-                  'message (conc "Invalid session ID: " sid))
-                 (make-property-condition
-                  'invalid-session)))
-         #f)
-     ((session-storage-ref) (session-table) sid)))
+;; Session items
+(define-record session-item expiration ip bindings finalizer)
 
-  
-  (define (make-session-table)
-    ;; The default value is a in-memory hash-table whose format is
-    ;; key=sid value=#<session-item expiration from-ip bindings>
-    ((session-storage-initialize)))
+(define (get-session-item sid #!optional (error? #t))
+  (handle-exceptions
+      exn
+    (if error?
+        (raise (make-composite-condition
+                (make-property-condition
+                 'exn
+                 'message (conc "Invalid session ID: " sid))
+                (make-property-condition
+                 'invalid-session)))
+        #f)
+    ((session-storage-ref) (session-table) sid)))
 
-  (define session-table (make-parameter (make-session-table)))
 
-  (define match-ip-address? (make-parameter #f))
+(define (make-session-table)
+  ;; The default value is a in-memory hash-table whose format is
+  ;; key=sid value=#<session-item expiration from-ip bindings>
+  ((session-storage-initialize)))
 
-  (define session-lifetime (make-parameter 3600)) ; 1h
+(define session-table (make-parameter (make-session-table)))
 
-  (define (expiration)
-    (+ (current-milliseconds)
-       (inexact->exact (floor (* (session-lifetime) 1000)))))
+(define match-ip-address? (make-parameter #f))
 
-  (define (session-create #!optional (bindings '()))
-    (let ((sid (unique-id))
-          (expiration (expiration)))
-      ((session-storage-set!) (session-table) sid (make-session-item expiration (remote-address) bindings #f))
-      (thread-start!
-       (make-thread
-        (lambda ()
-          (let loop ()
-            (let* ((sitem (get-session-item sid))
-                   (timeout (- (session-item-expiration sitem) (current-milliseconds))))
-              (when (> timeout 0)
-                    (thread-sleep! (/ timeout 1000))
-                    (loop))))
-          ((session-storage-delete!) (session-table) sid))))
-      sid))
+(define session-lifetime (make-parameter 3600)) ; 1h
 
-  (define (session-refresh! sid)
-    (session-item-expiration-set! (get-session-item sid) (expiration)))
+(define (expiration)
+  (+ (current-milliseconds)
+     (inexact->exact (floor (* (session-lifetime) 1000)))))
 
-  (define (session-valid? sid)
-    (not (not (and sid
-                   (and-let* ((sitem (get-session-item sid #f)))
-                     (if (match-ip-address?)
-                         (equal? (remote-address) (session-item-ip sitem))
-                         #t))))))
+(define (session-create #!optional (bindings '()))
+  (let ((sid (unique-id))
+        (expiration (expiration)))
+    ((session-storage-set!) (session-table) sid (make-session-item expiration (remote-address) bindings #f))
+    (thread-start!
+     (make-thread
+      (lambda ()
+        (let loop ()
+          (let* ((sitem (get-session-item sid))
+                 (timeout (- (session-item-expiration sitem) (current-milliseconds))))
+            (when (> timeout 0)
+              (thread-sleep! (/ timeout 1000))
+              (loop))))
+        ((session-storage-delete!) (session-table) sid))))
+    sid))
 
-  (define (session-destroy! sid)
-    (let ((finalizer (session-item-finalizer (get-session-item sid))))
-      (when finalizer (finalizer sid)))
-    ((session-storage-delete!) (session-table) sid))
+(define (session-refresh! sid)
+  (session-item-expiration-set! (get-session-item sid) (expiration)))
 
-  (define session-delete!  ;; DEPRECATED
-    session-destroy!)
+(define (session-valid? sid)
+  (not (not (and sid
+                 (and-let* ((sitem (get-session-item sid #f)))
+                   (if (match-ip-address?)
+                       (equal? (remote-address) (session-item-ip sitem))
+                       #t))))))
 
-  (define session-id-generator
-    (make-parameter
-     (lambda ()
-       (string->sha1sum
-        (conc (current-milliseconds)
-              (current-process-id)
-              (random (+ 1000 (current-process-id))))))))
+(define (session-destroy! sid)
+  (let ((finalizer (session-item-finalizer (get-session-item sid))))
+    (when finalizer (finalizer sid)))
+  ((session-storage-delete!) (session-table) sid))
 
-  (define (unique-id)
-    (let try-again ((id ((session-id-generator))))
-      (if (session-valid? id)
-          (try-again ((session-id-generator)))
-          id)))
+(define session-delete!  ;; DEPRECATED
+  session-destroy!)
 
-  (define (session-ref sid var #!optional default)
-    (or (alist-ref var (session-item-bindings (get-session-item sid)))
-        default))
+(define session-id-generator
+  (make-parameter
+   (lambda ()
+     (string->sha1sum
+      (conc (current-milliseconds)
+            (current-process-id)
+            (random (+ 1000 (current-process-id))))))))
 
-  (define (session-set! sid var val)
-    (let* ((sitem (get-session-item sid))
-           (new-bindings (alist-update! var val (session-item-bindings sitem))))
-      (session-item-bindings-set! sitem new-bindings)))
+(define (unique-id)
+  (let try-again ((id ((session-id-generator))))
+    (if (session-valid? id)
+        (try-again ((session-id-generator)))
+        id)))
 
-  (define (session-set-finalizer! sid proc)
-    (session-item-finalizer-set! (get-session-item sid) proc))
+(define (session-ref sid var #!optional default)
+  (or (alist-ref var (session-item-bindings (get-session-item sid)))
+      default))
 
-  (define (session-bindings sid)
-    (session-item-bindings (get-session-item sid)))
+(define (session-set! sid var val)
+  (let* ((sitem (get-session-item sid))
+         (new-bindings (alist-update! var val (session-item-bindings sitem))))
+    (session-item-bindings-set! sitem new-bindings)))
 
-  (define (session-del! sid var)
-    (alist-delete! var (session-bindings sid)))
+(define (session-set-finalizer! sid proc)
+  (session-item-finalizer-set! (get-session-item sid) proc))
 
-  (define session-delete-binding! ;; DEPRECATED
-    session-del!)
+(define (session-bindings sid)
+  (session-item-bindings (get-session-item sid)))
 
-  (define (session-cleanup!)
-    ((session-storage-cleanup!)))
-  
-  )
+(define (session-del! sid var)
+  (alist-delete! var (session-bindings sid)))
+
+(define session-delete-binding! ;; DEPRECATED
+  session-del!)
+
+(define (session-cleanup!)
+  ((session-storage-cleanup!)))
+
+) ;; end module
